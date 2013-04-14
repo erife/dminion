291ff3c37a7bc0cb2f195a6ccb3786d4e7914c31
Peter Stradinger <peter@rightscale.com>
2013-04-14 12:04:38 -0700

    refactored everything to be modular. Gonna take a shot at file separation.

--- public/js/dm.js
+++ public/js/dm.js
@@ -1,10 +1,10 @@
-$(function(){
-  pad = function(str, max) {
-    return str.length < max ? pad("0" + str, max) : str;
-  }
+var DMINION = (function(){
+  var my ={};
+  // utility functions
+  var ws = new WebSocket("ws://0.0.0.0:8080");
 
-  ws = new WebSocket("ws://0.0.0.0:8080");
 
+  // Web socket event handling
   ws.onmessage = function(evt) {
     var data = $.parseJSON(evt.data);
 
@@ -27,10 +27,19 @@ $(function(){
     handle_initiative_change();
   };
 
+  var pad = function(str, max) {
+    return str.length < max ? pad("0" + str, max) : str;
+  },
+
+  playSound = function( url ){
+    document.getElementById("sound").innerHTML="<embed src='"+url+"' hidden=true autostart=true loop=false>";
+  },
+
+
   handle_attr_change_health = function(data) {
     var selector = "#" + data["character_name"] + " .health";
     $(selector).html(data["msg"]);
-  };
+  },
 
   handle_attr_change_status = function(data) {
     var character_name = data["character_name"];
@@ -41,20 +50,37 @@ $(function(){
       $("#" + character_name + " td.status").removeClass("connected");
     }
 
-  };
+  },
 
   emit_event = function(type, character_name, msg){
     ws.send(JSON.stringify({"type": type, "character_name": character_name, "msg": msg}));
-  }
+  },
+
 
   emit_status_event = function(character_name, msg){
     emit_event("status", character_name, msg);
-  }
+  },
 
   emit_health_event = function(character_name, msg){
     emit_event("health", character_name, msg);
-  }
+  },
 
+  // UI
+
+  // Action Log Panel
+  handle_action_log = function(evt){
+    var form = $(evt.target),
+    input = form.children("input");
+
+    emit_event("action_log", "dm", input.val());
+    input.val("");
+    input.blur();
+
+    return false;
+
+  },
+
+  // Turn Management Panel
   next_character = function(){
     var current_character = $("tr.current_turn"),
     next_character = current_character.next();
@@ -72,9 +98,13 @@ $(function(){
 
     next_character.focus();
 
-  }
+  },
 
-  function heal_character(e){
+  // Resolution Panel
+
+  heal_character = function(e){
+    console.log("heaing");
+
     var target = $("#heal"),
     value = target.val(),
     current_targets = get_current_targets();
@@ -93,9 +123,9 @@ $(function(){
       target.val("");
     }
     return false;
-  }
+  },
 
-  function damage_character(e){
+  damage_character = function(e){
     var target = $("#damage"),
     value = target.val();
 
@@ -109,86 +139,70 @@ $(function(){
 
       })
 
-      boilerplate = character_name + " damages " + targets_string(current_targets) + " for " + value + " health";
+      boilerplate = character_name + " does " + value + " damage to " + targets_string(current_targets);
       auto_populate_action_log(boilerplate);
 
       // clear the input
       target.val("");
     }
     return false;
-  }
+  },
 
-
-
-  function get_current_targets(){
+  get_current_targets = function(){
     var targets = $("tr.target").map(function(){ return $(this).attr("id");}).get();
     return targets;
-  }
+  },
 
-  function targets_string(targets){
+  targets_string = function(targets){
     if(targets.length > 1){
       var display = targets.slice(0,-1).join(", ") + " and " + targets.slice(-1);
     } else{
       var display = targets[0];
     }
     return display;
-  }
+  },
 
-  $("#apply_heal").submit(heal_character);
-  $("#apply_damage").submit(damage_character);
-
-  initialize = function(){
-    next_character();
-    $(".character input").blur(function(){next_character();})
-  }
-
-  function handlekey(event){
-
+  handlekey = function(event){
+    var k_del = 8,
+    k_backspace = 46,
+    k_spacebar = 32,
+    k_ctrl = 17,
+    k_1 = 49,
+    k_9 = 57,
+    k_0 = 48,
+    k_n = 78,
+    k_h = 72,
+    k_d = 68;
 
     // Handle delete events to prevent back button from OSX
-    if (event.which == 8 || event.which == 46)
-    {
-        if(event.target.localName != "input") {
-	        return false;
-        }
-    }
+    if (event.which == k_del || event.which == k_backspace)
+    {if(event.target.localName != "input") {return false;}}
     // Handle spacebar
-    else if(event.which == 32){
-
-    }
+    else if(event.which == k_spacebar){}
     // Esc
-    else if(event.which == 27){
-    }
+    else if(event.which == 27){}
     else if (event.ctrlKey){
       event.preventDefault();
 
-      if(event.ctrlKey && event.which != 17){
+      if(event.ctrlKey && event.which != k_ctrl){
         console.log(event.which);
         // Numbers 1-9 for selecting targets
-        if(49 <= event.which && event.which <= 57){
+        if(k_1 <= event.which && event.which <= k_9){
           var target_index = String.fromCharCode(event.which);
           toggle_target_by_index(target_index);
         }
 
         // clear all selected targets
-        if(event.which == 48){
-          clear_targets();
-        }
+        if(event.which == k_0){clear_targets();}
 
         // C-n for moving to the next turn
-        if(event.which == 78){
-            next_character();
-          }
+        if(event.which == k_n){next_character();}
 
         // C-d for applying damage
-        if(event.which == 72){
-          $("#heal").focus();
-        }
+        if(event.which == k_h){$("#heal").focus();}
 
         // C-h for healing
-        if(event.which == 68){
-          $("#damage").focus();
-          }
+        if(event.which == k_d){$("#damage").focus();}
 
       }
     }
@@ -201,7 +215,7 @@ $(function(){
     // unhandled
     else{
     }
-  }
+  },
 
   handle_expire_phase = function(evt){
     var target = $(evt.target),
@@ -213,15 +227,15 @@ $(function(){
     // prepopulate the action log with a message
     boilerplate = current_character_name() + " " + msg;
     auto_populate_action_log(boilerplate);
-  }
+  },
 
-   handle_initiative_change = function(evt) {
+  handle_initiative_change = function(evt) {
     // a simple compare function, used by the sort below
-     var compare_rows = function (a,b){
-       var a_val = parseInt($(a).children(".initiative").html());
-       var b_val = parseInt($(b).children(".initiative").html());
+    var compare_rows = function (a,b){
+      var a_val = parseInt($(a).children(".initiative").html());
+      var b_val = parseInt($(b).children(".initiative").html());
 
-       if (a_val>b_val){
+      if (a_val>b_val){
         return -1;
       }
       if (a_val<b_val){
@@ -231,63 +245,39 @@ $(function(){
     };
 
     // the actual sort
-     $('#party table .character')
-       .sort(compare_rows)
-       .appendTo('#party table')
-       .map(function(i){
-         var index = pad(new String(i+1), 2);
+    $('#party table .character')
+      .sort(compare_rows)
+      .appendTo('#party table')
+      .map(function(i){
+        var index = pad(new String(i+1), 2);
 
-         $(this).addClass("creature_" + index);
-       });
+        $(this).addClass("creature_" + index);
+      });
 
 
-  };
-
-  handle_action_log = function(evt){
-    var form = $(evt.target),
-    input = form.children("input");
-
-    emit_event("action_log", "dm", input.val());
-    input.val("");
-    input.blur();
-
-    return false;
-
-  }
+  },
 
   target_creature = function(evt){
     var creature = $(evt.target).parents("tr");
     creature.toggleClass("target");
-  }
+  },
 
   toggle_target_by_index = function(index){
     var creature = $(".creature_" + pad(index, 2));
     creature.toggleClass("target");
-  }
+  },
 
   clear_targets = function(){
     $("tr.target").removeClass("target");
-  }
+  },
 
   current_character_name = function(){
     return $("tr.current_turn").attr("id");
-  }
+  },
 
   auto_populate_action_log = function(msg) {
     $("#action_form input").val(msg + " ").focus();
-  }
-
-  $("#action img.phase").click(handle_expire_phase);
-  $("#action_form").submit(handle_action_log);
-
-
-  $(".character img.target").click(target_creature);
-
-  $(document).keydown(handlekey);
-
-
-  initialize();
-
+  },
 
   set_action_graph = function(chosen_action){
 
@@ -327,7 +317,7 @@ $(function(){
 
     $("#difficulty_check").attr("src", url);
     $("#damage").focus();
-  }
+  },
 
   handle_choose_action = function(evt) {
     var chosen_action = $(evt.target).html();
@@ -338,11 +328,24 @@ $(function(){
     $("#difficulty_check").removeClass("hidden");
 
     console.log(evt);
+  };
+
+  my.initialize = function(){
+    next_character();
+    // map event handlers
+    $(".character input").blur(function(){next_character();})
+    $("#action img.phase").click(handle_expire_phase);
+    $("#action_form").submit(handle_action_log);
+    $("#apply_heal").submit(heal_character);
+    $("#apply_damage").submit(damage_character);
+    $(".character img.target").click(target_creature);
+    $("#available li").click(handle_choose_action);
+
+    $(document).keydown(handlekey);
   }
+  return my;
+}());
 
-  $("#available li").click(handle_choose_action);
-
-  playSound = function( url ){
-  document.getElementById("sound").innerHTML="<embed src='"+url+"' hidden=true autostart=true loop=false>";
-}
-});
+$(function(){
+  DMINION.initialize();
+})
