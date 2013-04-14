db96ce8221b6dfc1dddae5b989730f91ccaef476
Peter Stradinger <peter@rightscale.com>
2013-04-13 21:02:24 -0700

    added some characters to the list and some formatting cleanup

--- db_setup.rb
+++ db_setup.rb
@@ -8,7 +8,9 @@ statements = [
   '''drop table characters ''',
   '''create table characters (name, health, max_health, initiative, role) ''',
   '''insert into characters (name, health, max_health, initiative, role) values ("Zakiki", 56, 60, 2, "mage")''',
-  '''insert into characters (name, health, max_health, initiative, role) values ("Santorini", 65, 68, 12, "ranger")'''
+  '''insert into characters (name, health, max_health, initiative, role) values ("Santorini", 65, 68, 12, "ranger")''',
+  '''insert into characters (name, health, max_health, initiative, role) values ("Pook", 45, 45, 28, "rogue")''',
+  '''insert into characters (name, health, max_health, initiative, role) values ("Toronaga", 87, 87, 23, "warrior")'''
 ]
 
 statements.each do |statement|
